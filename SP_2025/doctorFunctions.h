59f4b2580f704f397d43747095177228706a8959
min1magg1t <judeswork777@gmail.com>
2025-04-25 17:11:03 +0300

    Testing SP_2025/doctorFunctions.h

--- SP_2025/doctorFunctions.h
+++ SP_2025/doctorFunctions.h
@@ -3,6 +3,8 @@
 
 #include "structs.h"
 
+// PLEASEEEEEEEEEEEEEEEEEEEEEEE 2E4T8ALLLLLLLLLLLLLLLL
+
 int getNumTimeSlots(int loggedDocIndex);
 void displayDocListAvail(int DocIndex, int availableIndexes[], int& count, int slot = -1);
 void editTime(int loggedDocIndex);
