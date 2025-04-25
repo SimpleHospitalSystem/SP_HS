eaf2e1450a64c1c5df84d1ecfbd96da7e0ad95b5
Alaa SuperGirl <alaahaitham653@gmail.com>
2025-04-25 20:25:06 +0300

    Added addAvailTimeDoc() & existAvailableTimeSlot() and other fns

--- SP_2025/doctorFunctions.h
+++ SP_2025/doctorFunctions.h
@@ -7,8 +7,12 @@
 
 int getNumTimeSlots(int loggedDocIndex);
 void displayDocListAvail(int DocIndex, int availableIndexes[], int& count, int slot = -1);
+void validateAvailTime(int loggedDoc, int Index);
 void editTime(int loggedDocIndex);
-int getNumTimeSlots(int loggedDocIndex);
+int getNumApptSlot(int loggedDoc);
 void viewDocAppt(int loggedDoc);
+void addAvailableTimeDoc(int loggedDocIndex);
+bool existAvailableTimeSlot(int loggedDocIndex, int& newTimeIndex);
+void getInput(int& time);
 
 #endif
