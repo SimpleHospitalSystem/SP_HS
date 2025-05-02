85398c03d6eadc970a4f4d7939b5b4446db32c4d
min1magg1t <judeswork777@gmail.com>
2025-05-02 12:38:17 +0300

    Main menu Sample

--- SP_2025/otherFunctions.cpp
+++ SP_2025/otherFunctions.cpp
@@ -2,3 +2,38 @@
 #include <iomanip>
 #include "otherFunctions.h"
 using namespace std;
+
+void mainMenu() {
+    int choice;
+    do {
+        cout << "\nHospital Management System\n";
+        cout << "1. Register as Doctor\n";
+        cout << "2. Register as Patient\n";
+        cout << "3. Login as Doctor\n";
+        cout << "4. Login as Patient\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice) {
+        case 1:
+            // registerDoctor(doctors, numDoctors);
+            break;
+        case 2:
+            // registerPatient(patients, numPatients);
+            break;
+        case 3:
+            // loginDoctor(doctors, numDoctors);
+            break;
+        case 4:
+            // loginPatient(patients, numPatients);
+            break;
+        case 5:
+            // saveDoctors(doctors, numDoctors);
+            // savePatients(patients, numPatients);
+            cout << "Data saved. Goodbye!\n";
+            break;
+        default:
+            cout << "Invalid choice. Try again.\n";
+        }
+    } while (choice != 0);
+}
